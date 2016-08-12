b436d261954a4e740858fee557ee2aab5733cce9
kassHa <katarzynae.lach@gmail.com>
2016-08-11 23:31:31 -0700

    pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+# First create a hash
+# Ask user for the input
+# Convert input to appropriate data type (integers, booleans)
+# When application is finished, print it to the screen
+# Use the if statement to find out if the user made a mistake, and wants to update it.
+# If user wants to update the input, and ask which label/key needs to be updated.
+# Ask the user to type a new value
+# Print hash to the screen  
