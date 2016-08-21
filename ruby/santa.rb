2b318f8d0efde5a109bf7a31ecfb5bd36e3ee966
kassHa <katarzynae.lach@gmail.com>
2016-08-21 01:10:57 -0700

    Added driver code to create MANY santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -25,18 +25,22 @@ class Santa
 	end
 
 	def get_mad_at(name_reindeer) #Method that takes name of the reindeer (string) and puts that name in the last position of internal @reindeer_ranking
-	    position_in_array = @reindeer_ranking(name_reindeer) #Find current index of the reindeer name
+	    position_in_array = @reindeer_ranking.index(name_reindeer) #Find current index of the reindeer name
 	    last_index_in_array = (@reindeer_ranking.length)-1 #Find the last index in the array. Substracting 1 because arrays are indexed from 0 so last index is actually length of array -1
 	    @reindeer_ranking[position_in_array] = @reindeer_ranking[last_index_in_array] #Assign value from the last index in the array, to the position where provided name_reindeer was
 	    @reindeer_ranking[last_index_in_array] = name_reindeer # Put provided name_reindeer in the last position in the array
 	end
 
 	def print_reindeer_favs #Method that prints reindeer ranking for santa
-		puts reinder_ranking
+		puts @reindeer_ranking
 	end
 
 	def gender=(new_gender)	#Method that takes new gender as input (string) and reasigns it to @gender attribute. We can use it simply by name_instance.gender = "new_name"
 		@gender = new_gender
+	end
+
+	def gender #Method that will return gender of santa
+		return @gender
 	end
 
 	def age #Method that returns age of the santa
@@ -56,13 +60,44 @@ end
 #santas << Santa.new("male","asian")
 
 #Test new methods to make sure everything works
-santa1 = Santa.new("male","white")
-puts santa1.age
-puts santa1.ethnicity
-santa1.celebrate_birthday
-puts santa1.age
+#santa1 = Santa.new("male","white")
+#puts santa1.age
+#puts santa1.ethnicity
+#santa1.celebrate_birthday
+#puts santa1.age
+#santa1.set_age(20)
+#puts santa1.age
+#santa1.gender = "female"
+#puts santa1.gender
+#santa1.get_mad_at("Rudolph")
+#santa1.print_reindeer_favs
 
 
+# Create multiple unique santas
+santas = []  # Array to store unique santas
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"] # example array with different genders
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"] #example array with different ethnicities
+
+
+1000000.times do |santa_instance| # Create 1000 new instances of santas and store in arrat
+	santa_instance =  Santa.new(example_genders.sample,example_ethnicities.sample) # Create new santa with randomly selected gender and ethnicity
+	random_number = Random.new #Select new random number
+	santa_instance.set_age(random_number.rand(140)) #Set new age of santa instance using random number
+	santas << santa_instance # Add santa instance to an array of santas instances
+end
+
+i = 1 # Create index so we can print number of each santa
+#Print attributes of each unique santa
+santas.each do |santa_instance|
+	puts "Santa#{i}"
+	santa_instance.speak
+	puts "Age:#{santa_instance.age}"
+	puts "Ethnicity:#{santa_instance.ethnicity}"
+	puts "Gender:#{santa_instance.gender}"
+	i+=1 #increment index by 1 
+end
+
+# Wow we created 1000000 santas! on my mac air it takes about 2 minutes to do :)
 
 
 
