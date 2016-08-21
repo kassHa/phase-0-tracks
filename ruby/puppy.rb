2310ab6805ab357dee43447e11ca6b772482ed83
kassHa <katarzynae.lach@gmail.com>
2016-08-20 20:59:41 -0700

    Puppy class created

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+# Let's create a puppy class
+
+#SPECIES----------------------------
+# Puppies
+
+#CHARACTERISTICS--------------------
+#Number of ears: 2
+#Number of paws: 4
+#Friendly: absolutely
+#Cute: yes
+#Name: varies
+#Color: varies
+#Playful: yes
+#Sleepy: yes
+
+#BEHAVIOR---------------------------
+#Jump
+#Lick
+#Play
+#Fetch
+#Sleep
+#Bite
+#Eat
+#Run
