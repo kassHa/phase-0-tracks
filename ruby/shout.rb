abf8d2e1388a75118a82622a89abffdb909fbefe
kassHa <katarzynae.lach@gmail.com>
2016-08-21 15:48:57 -0700

    Mixin_module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,6 @@
+
+#Standalone module Shout
+=begin
 module Shout 
 	
 	def self.yell_angrily(words)
@@ -9,6 +12,35 @@ module Shout
 	end
 
 end
+=end
 
-puts Shout.yell_happily("kassHa")
-puts Shout.yell_angrily("kassHa")
+#Mixin module Shout
+module Shout 
+	
+	def yell_angrily(words)
+		return (words+"!!!" + " :(")
+	end
+
+	def yell_happily(words)
+		return (words+"!!!" + " :)")
+	end
+
+end
+
+class Husband
+	include Shout
+end
+
+class Wife
+	include Shout
+end
+
+husband = Husband.new
+wifey = Wife.new
+
+
+puts husband.yell_happily("Babes")
+puts husband.yell_angrily("Babes")
+puts wifey.yell_angrily("Honey")
+puts wifey.yell_happily("Honey")
+
