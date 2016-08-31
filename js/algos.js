89174de2d867d6e7037a2c07a2f99365638271c9
kassHa <katarzynae.lach@gmail.com>
2016-08-31 12:38:43 -0700

    pseudocode

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Pseudocode: Release 0
+// Function input: an array
+// Function output: longest word from the array(input)
+// Create a variable to hold the biggest word in, keep it empty at first
+// Loop through an array of words/phrases and calculate the length
+// of each word or phrase
+// If the length of the array word/phrase is longer than the length of
+// the variable we update the variable
+// Otherwise, the variable is not updated and we continue to loop through 
+// the rest of the array
