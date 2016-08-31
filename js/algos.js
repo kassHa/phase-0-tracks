96a557a7f42636403566ba5e2e4194cf0607c1be
kassHa <katarzynae.lach@gmail.com>
2016-08-31 12:40:00 -0700

    release 1

--- js/algos.js
+++ js/algos.js
@@ -8,3 +8,43 @@
 // the variable we update the variable
 // Otherwise, the variable is not updated and we continue to loop through 
 // the rest of the array
+
+function findLongestWord(array) {
+  var longestWord = ""; 
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > longestWord.length) {
+      var longestWord = array[i];
+    } else {
+      var longestWord;
+    }
+  };
+
+  return longestWord;
+}
+
+
+// Pseudocode: Release 1
+// Function input: 2 objects
+// Function output: true or false
+// Create 2 variables to hold an array of keys for each object
+// (Assume both objects have the same number of keys)
+// Loop through the first object's keys and get the key name at every iteration.
+// Then using the key name, check if the value for that key is the same in both objects
+// If both values match then return true
+// Otherwise, continue checking for the next key.
+// When the loop ends and no key/value match, then return false
+
+function keyValueMatch(obj1, obj2) {
+  var obj1Keys = Object.keys(obj1);
+  var obj2Keys = Object.keys(obj2);
+
+  for (var i = 0; i < obj1Keys.length; i++) {
+    var key = obj1Keys[i];
+    if(obj1[key] === obj2[key]) {
+      return true;
+    }
+  }
+
+  return false;
+}
